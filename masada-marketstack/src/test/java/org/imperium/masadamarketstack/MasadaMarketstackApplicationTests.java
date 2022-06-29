6aaa43775e764374b62819fb3be23fbe1f87c5b3
GermanPlaxunov <snipe9029quant@gmail.com>
2022-06-30 00:49:16 +0300

    ver 0.0 --> marketstack module added.

---
+++ masada-marketstack/src/test/java/org/imperium/masadamarketstack/MasadaMarketstackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imperium.masadamarketstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasadaMarketstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
