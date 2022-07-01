c3d0d6fc598a00a42ed5bb8f543e75b0d781754b
GermanPlaksunov <snipe9029quant@gmail.com>
2022-07-01 20:00:08 +0300

    ver 0.0 --> masada-engine added.

---
+++ masada-engine/src/main/java/org/imperium/masadaengine/MasadaEngineApplication.java
@@ -0,0 +1,13 @@
+package org.imperium.masadaengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasadaEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasadaEngineApplication.class, args);
+    }
+
+}
---
+++ masada-engine/src/test/java/org/imperium/masadaengine/MasadaEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imperium.masadaengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasadaEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
